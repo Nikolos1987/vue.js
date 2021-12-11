2b98367575236e5a069333bd02f3efe402ce66b5
archer1987 <nikolos04051987@gmail.com>
2021-12-11 12:23:42 +0700

    lesson__1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="hello">
+  <input v-model.number="operand1"/>
+       <input v-model.number="operand2"/> 
+    <div class="keyboard">
+         <button v-on:click="summ">+</button>
+         <button v-on:click="diff">-</button>
+         <button v-on:click="division">/</button>
+         <button v-on:click="mult">*</button>
+         <button v-on:click="pow">степень числа</button>
+         <button v-on:click="natDel">%</button>
+     </div>
+     ={{result}}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data(){
+       return {
+           operand1: 1,
+           operand2: 0,
+           result: 0
+       }
+   },
+   methods:{
+     summ(){
+      this.result=this.operand1+this.operand2
+     },
+     diff(){
+       this.result=this.operand1-this.operand2
+     },
+     division(){
+       if(this.operand2==0){
+         this.result="На ноль делить нельзя"
+       }
+       else{this.result=this.operand1/this.operand2} 
+     
+     },
+     mult(){
+       this.result=this.operand1*this.operand2
+     },
+     pow(){
+       this.result=Math.pow(this.operand1,this.operand2)
+     },
+     natDel(){
+       this.result=this.operand1%this.operand2
+     }
+
+
+
+   }
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
