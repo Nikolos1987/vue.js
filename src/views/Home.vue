9c48b8e2face8419c2e7eae3a1ddbbefeba7bc8d
archer1987 <nikolos04051987@gmail.com>
2021-12-22 18:23:38 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,21 +3,20 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
 
-    <calc msg="Welcome to Your calc App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/calcul.vue'
-import calc from '@/components/calc.vue'
+
 
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
-    calc
+
 
   }
 }
