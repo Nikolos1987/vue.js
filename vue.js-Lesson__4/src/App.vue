7a062b96591a154225df7f70ce1bd4a4ac309052
Nikolos1987 <nikita.malyshev.87@mail.ru>
2022-01-11 12:23:49 +0700

    error

---
+++ vue.js-Lesson__4/src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="wrapper">
+    <header>
+      <div class="title">My personal costs</div>
+   
+    </header>
+    <main>          
+      <addpayment @addpayment="addPayData" />
+      <payment :items="paymentsList" />
+       <!-- <div class="total" v-if="total"> total {{total}}</div> -->
+    </main>
+  </div>
+</template>
+<script>
+import payment from "./components/payment.vue";
+import addpayment from "./components/addPayment.vue";
+import {mapMutations,mapGetters} from 'vuex' 
+
+export default {
+  components: { payment, addpayment },
+  name: "App",
+  data() {
+    return {
+      show: true,  
+    };
+  },
+  computed:{
+...mapGetters({paymentsList:"getPaymentLIstAll"}),  
+ total(){
+    return this.$store.getters.getPaymentLIstAll
+ }
+  },
+  methods: {
+    ...mapMutations("setPaymentListData"),
+    addPayData(data) {
+      this.paymentsList.push(data)
+    },
+    fetchData() {
+      return [
+        {
+          date: "28.03.2020",
+          category: "Food",
+          value: 169,
+        },
+        {
+          date: "24.03.2020",
+          category: "Transport",
+          value: 360,
+        },
+        {
+          date: "24.03.2020",
+          category: "Food",
+          value: 532,
+        },
+      ];
+    },
+  },
+  created() { 
+    this.$store.dispatch('fetchData')
+   this.$store.commit('setPaymentListData',this.fetchData())
+  },
+};
+</script>
+
+
+<style lang="scss">
+.title{
+  font-size: 20px;
+
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ vue.js-Lesson__4/src/store/index.js
@@ -0,0 +1,45 @@
+
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    paymentsList: []
+  },
+  mutations: {
+    setPaymentListData(state, payload) {
+      state.paymentsList = payload
+    },
+    addList(state, payload) {
+      state.paymentsList.push(payload)
+    }
+  },
+  getters: {
+    getPaymentLIst: (state) => {
+      return state.paymentsList.reduce((acc, cur) => acc + cur.value, 0)
+    },
+    getPaymentLIstAll: state => state.paymentsList,
+  },
+  actions: {
+    fetchData({commit}){
+     return new Promise(resolv=>{
+       setTimeout(()=>{
+         const items=[]
+         for(let i=1;i<101;i++){
+           items.push({
+            date: '28.03.2020',
+            category: 'Food',
+            value: 169,
+           })
+         }
+         resolv(items);
+       },2000)
+     }).then(res=>commit('setPaymentListData',res))
+    }
+
+  }
+})
+
+
