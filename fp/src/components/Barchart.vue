5efb05e1ab4ff52c274a4f46a4bb353f404448fe
Nikolos1987 <85107815+Nikolos1987@users.noreply.github.com>
2022-01-30 17:18:45 +0700

    lesson8

---
+++ fp/src/components/Barchart.vue
@@ -0,0 +1,40 @@
+<script>
+    import Bar from 'vue-chartjs'
+    export default {
+      extends: Bar,
+      data (){ 
+          return{
+                chartData: {
+    labels: [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ],
+    datasets: [
+      {
+        label: 'Data One',
+        data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false
+  }
+          }
+      } ,
+        
+      mounted () {
+        this.renderChart(this.chartData, this.options)
+      }
+    }
+  </script>
--- fp/src/views/Dashboard.vue
+++ fp/src/views/Dashboard.vue
@@ -1,56 +1,39 @@
+
 <template>
-    <v-container>
-      <v-row>
-        <v-col>
-          <div class="text-h5 text-sm-h3 mb-8">My personal costs</div>
-          <v-dialog
-            v-model="dialog"
-            width="500"
-          >
-          <template v-slot:activator="{on}">
+  <v-container>
+    <v-row>
+      <v-col>
+        <div class="text-h5 text-sm-h3 mb-8">My personal costs</div>
+        <v-dialog v-model="dialog" width="500">
+          <template v-slot:activator="{ on }">
             <v-btn color="teal" dark v-on="on">
               ADD NEW COST <v-icon>mdi-plus</v-icon>
             </v-btn>
-            </template>
-              <add-payment-form @addNewPayment="dialog=false"/>
-            </v-dialog>
-              <!-- <my-button>
-                <template #icon>
-                  <v-icon>mdi-plus</v-icon>
-                </template>
-              Нажми меня
-                </my-button>
-              <my-button><template #default="{props}">{{props}}</template></my-button> -->
-          
-              <payments-display :items="currentElements" />
-              <!-- <pagination
-                :cur="page"
-                :n="n"
-                :length="paymentsList.length"
-                @paginate="changePage"
-              /> -->
-              <!-- <button @click="openModal">Add new cost +</button> -->
+          </template>
+          <add-payment-form @addNewPayment="dialog = false" />
+        </v-dialog>
+
+        <payments-display :items="currentElements" />
       </v-col>
-      <v-col>
-        chart
-      </v-col>
+      <v-col> <Barchart/> </v-col>
     </v-row>
   </v-container>
-
 </template>
 
 <script>
 import PaymentsDisplay from "../components/PaymentsDisplay.vue";
 import { mapMutations, mapGetters, mapActions } from "vuex";
 import AddPaymentForm from '../components/AddPaymentForm.vue';
-// import MyButton from '../components/MyButton.vue';
-// import Pagination from "../components/Pagination.vue";
+import Barchart from '../components/Barchart.vue';
+
 export default {
+
   name: "Dashboard",
   components: {
     PaymentsDisplay,
     AddPaymentForm,
-    // MyButton,
+    Barchart,
+   
   },
   data() {
     return {
@@ -74,6 +57,9 @@ export default {
       return this.paymentsList.slice(n * (page - 1), n * (page - 1) + n);
     },
   },
+  mounted () {
+    this.renderChart()
+  },
   methods: {
     ...mapMutations({ fetch: "setPaymentsListData" }),
     ...mapActions(["fetchData"]),
@@ -86,13 +72,15 @@ export default {
         header: "Add new cost",
       });
     },
+  
+
+    
   },
   async created() {
     if (this.$route.params?.page) {
       this.page = Number(this.$route.params.page);
     }
-    // this.$store.dispatch('fetchData');
-    // this.$store.commit('setPaymentsListData', this.fetchData());
+   
   },
 };
 </script>
